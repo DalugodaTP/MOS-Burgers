c0e88e0dbc10eef67bcbb02da697303fc5481446
Tharindu <Dalugodatp@gmail.com>
2024-01-01 12:11:22 +0530

    Added toasts

--- js/signUp.js
+++ js/signUp.js
@@ -1,44 +1,150 @@
 var customerList;
 
+//--------------------------Validation-------------------------------------
 
-document.getElementById('btnSignUp').addEventListener('click', (event) => {
+//save reference from each form item
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+    //prevent submitting in order to validate
+    e.preventDefault();
+    //validate all inputs
+    validateInput();
     
-    if (true) { //validate data
-        //capture the data from the fields
-        let firstName = document.getElementById('firstName').value;
-        let lastName = document.getElementById('lastName').value;
-        let email = document.getElementById('email').value;
-        let add = document.getElementById('address').value;
-        let passKey = document.getElementById('password').value;
-        let cardName = document.getElementById('cc-name').value;
-        let ccNum = document.getElementById('credit').value;
-        let expDate = document.getElementById('cc-expiration').value;
-        let cvvNum = document.getElementById('cc-cvv').value;
+    //--Default actions
+    let userName = document.username.value.trim();
+    let emailValue = email.value.trim();
+    let passwordValue = password.value.trim();
 
-        //store them into an object
-        let customerData = {
-            first_name: firstName,
-            last_name: lastName,
-            emailAdd: email,
-            address: add,
-            password: passKey,
-            Name_on_card: cardName,
-            cc_number: ccNum,
-            expirationDate: expDate,
-            cvv: cvvNum
-        };
+    //store them into an object
+    let customerData = {
+        user_name: userName,
+        emailAdd: emailValue,
+        password: passwordValue
+    };
 
-        // Add the data to the customerList array
-        //customerList.push(customerData);
+    customerList = customerData;
 
-        customerList = customerData;
+    //store that object in an array
+    addCustomerToMemory();
 
-        //store that object in an array
-        addCustomerToMemory();
+
+});
+
+//validate all input fields
+const validateInput = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    //--for username
+    if (usernameValue === '') {
+        setError(username, 'Username is required');
+    } else {
+        setSucess(username);
     }
-})
+
+    //--for email
+    if (emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Email is required');
+    } else {
+        setSucess(email);
+    }
+
+    //--for password
+    if (passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8) {
+        setError(password, 'Password must be at least 8 character.')
+    } else {
+        setSuccess(password);
+    }
+
+    //--seond password
+    if (password2Value === '') {
+        setError(password2, 'Please confirm your password');
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Passwords doesn't match");
+    } else {
+        setSuccess(password2);
+    }
+};
+
+const setError = (element, message) => {
+    //parent element
+    const inputControl = element.parentElement;
+    //arrow display
+    const errorDispay = inputControl.querySelector('.error');
+
+    errorDispay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+}
+
+const setSucess = element => {
+    //parent element
+    const inputControl = element.parentElement;
+    //arrow display
+    const errorDispay = inputControl.querySelector('.error');
+
+    errorDispay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+}
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+//--------------------------------------------------------------------------
 
 
+
+
+// document.getElementById('btnSignUp').addEventListener('click', (event) => {
+
+//     if (true) { //validate data
+//         //capture the data from the fields
+//         let firstName = document.getElementById('firstName').value;
+//         let lastName = document.getElementById('lastName').value;
+//         let email = document.getElementById('email').value;
+//         let add = document.getElementById('address').value;
+//         let passKey = document.getElementById('password').value;
+//         let cardName = document.getElementById('cc-name').value;
+//         let ccNum = document.getElementById('credit').value;
+//         let expDate = document.getElementById('cc-expiration').value;
+//         let cvvNum = document.getElementById('cc-cvv').value;
+
+//         //store them into an object
+//         let customerData = {
+//             first_name: firstName,
+//             last_name: lastName,
+//             emailAdd: email,
+//             address: add,
+//             password: passKey,
+//             Name_on_card: cardName,
+//             cc_number: ccNum,
+//             expirationDate: expDate,
+//             cvv: cvvNum
+//         };
+
+//         // Add the data to the customerList array
+//         //customerList.push(customerData);
+
+//         customerList = customerData;
+
+//         //store that object in an array
+//         addCustomerToMemory();
+//     }
+// })
 
 const addCustomerToMemory = () => {
     //if there is nothing saved at the start then save an empty array
@@ -55,28 +161,3 @@ const addCustomerToMemory = () => {
     console.log(JSON.parse(localStorage.getItem('customerDbList')));
 }
 
-
-
-//---Payment details---
-
-// Add event listener to each radio button
-document.getElementsByName('paymentMethod').forEach(function (radio) {
-    let nameLbl = document.getElementById('cardType');
-    radio.addEventListener('change', function () {
-        // Check which radio button is selected
-        if (radio.checked) {
-            switch (radio.id) {
-                case "credit": {
-                    nameLbl.innerHTML = `
-                <label for="cc-number" class="form-label">Credit card number</label>
-                <input type="text" class="form-control" id="cc-number-credit" placeholder="" required="">`;
-                }; break;
-                case "debit": {
-                    nameLbl.innerHTML = `
-                <label for="cc-number" class="form-label">Debit card number</label>
-                <input type="text" class="form-control" id="cc-number-debit" placeholder="" required="">`;
-                }; break;
-            }
-        }
-    });
-});
